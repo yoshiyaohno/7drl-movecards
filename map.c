c2d0fac3301e1d5f6834213f22434fe17ca82c8b
yoshiyaohno <yoshiyaohno@gmail.com>
2019-07-09 12:12:03 -0400

    add basic map

---
+++ map.c
@@ -0,0 +1,38 @@
+#include "map.h"
+
+Map *init_map()
+{
+    Map *m = malloc(sizeof(Map));
+    m->rows = 8;
+    m->cols = 16;
+
+    m->arr = malloc(m->rows * sizeof(char *));
+    int i, j;
+    for (i = 0; i < m->rows; ++i) {
+        m->arr[i] = malloc(m->cols * sizeof(char));
+        for (j = 0; j < m->cols; ++j) {
+            m->arr[i][j] = TILE_FLOOR;
+        }
+    }
+    for (i = 2, i < 5, ++i) {
+        for (j = 5, j < 9, ++j) {
+            m->arr[i][j] = TILE_WALL;
+        }
+    }
+    return m;
+}
+
+int is_valid(Map *m, int row, int col)
+{
+    if (row < 0 || row >= m->rows || col < 0 || col >= m->cols) return 0;
+    return TILE_FLOOR == m->arr[i][j];
+}
+
+void free_map(Map *m)
+{
+    int i;
+    for(i = 0; i < m->rows; ++i) free(m->arr[i]);
+    free(m->arr);
+    free(m);
+}
+
---
+++ map.h
@@ -0,0 +1,24 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum tile_t {
+    TILE_FLOOR,
+    TILE_WALL,
+} tile_t;
+
+typedef struct Map {
+    tile_t **arr;
+    int rows, cols;
+} Map;
+
+Map *init_map();
+
+void free_map(Map *);
+
+int is_valid(Map *, int row, int col);
+
+#endif
+
